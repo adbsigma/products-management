cf4f9f52de7353ecb4fae522d3f7166c3fd387e3
adbsigma <adbsigma@gmail.com>
2018-11-03 17:03:39 +0800

    wip

--- src/main/java/com/adbsigma/productsmanagement/web/controller/ProductController.java
+++ src/main/java/com/adbsigma/productsmanagement/web/controller/ProductController.java
@@ -1,7 +1,16 @@
 package com.adbsigma.productsmanagement.web.controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
 
+    @GetMapping(value="/Produits")
+    public String listeProduits() {
+        return "Un exemple de produit";
+    }
+
+    @GetMapping(value = "/Produits/{id}")
+    public String afficherUnProduit(@PathVariable int id) {
+        return "Vous avez demandé un produit avec l'id  " + id;
+    }
 }
